3385f62e83941b384104a69d7c0e081971c68484
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-30 14:34:20 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dg_calibration',
-    version='0.3',
+    version='0.3.1',
     description='Calibration constants and functions for DitialGlobe fleet',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
